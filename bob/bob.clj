a64836474d68a7b01985929175ed0ead18a6d3e5
alvaro garcia <derecerca@gmail.com>
2015-09-14 22:12:31 +0200

    refactor: extract function to re-matches with anything around the pattern 'contains'

--- bob/bob.clj
+++ bob/bob.clj
@@ -3,9 +3,13 @@
 (defn- has-question? [question]
   (re-matches #".*\?" question))
 
+
+(defn- re-matches-contains [regex text]
+  (re-matches (re-pattern (str ".*" regex ".*")) text))
+
 (defn- is-shouting? [question]
-  (and (re-matches #".*[A-Z]+.*" question)
-       (not (re-matches #".*[a-z]+.*" question))))
+  (and (re-matches-contains "[A-Z]+" question)
+       (not (re-matches-contains "[a-z]+" question))))
 
 (defn- blank? [question]
   (= "" (clojure.string/trim question)))
