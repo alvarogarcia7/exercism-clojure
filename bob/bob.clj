a7b1a7dffa46736fd483c982b091ca26dd95c972
alvaro garcia <derecerca@gmail.com>
2015-09-14 22:05:10 +0200

    green: solve blank or empty questions

--- bob/bob.clj
+++ bob/bob.clj
@@ -7,10 +7,14 @@
   (and (re-matches #".*[A-Z]+.*" question)
        (not (re-matches #".*[a-z]+.*" question))))
 
+(defn- blank? [question]
+  (= "" (clojure.string/trim question)))
+
    
 (defn response-for [question]
   (cond
     (is-shouting? question) "Whoa, chill out!"
     (has-question? question) "Sure."
+    (blank? question) "Fine. Be that way!"
     :else "Whatever."))
 
