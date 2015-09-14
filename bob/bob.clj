730c6d370715a94c7229147cd1e243e61e965343
alvaro garcia <derecerca@gmail.com>
2015-09-14 21:55:13 +0200

    green: solve questios in upper-case

--- bob/bob.clj
+++ bob/bob.clj
@@ -3,9 +3,13 @@
 (defn- has-question? [question]
   (<= 1 (count (filter #(= "?" %) (clojure.string/split question #"")))))
 
+(defn- is-shouting? [question]
+  (not (re-matches #".*[a-z]+.*" question)))
+
    
 (defn response-for [question]
   (cond
+    (is-shouting? question) "Whoa, chill out!"
     (has-question? question) "Sure."
     :else "Whatever."))
 
